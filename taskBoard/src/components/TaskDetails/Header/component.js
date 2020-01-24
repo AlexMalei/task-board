224633a64532d1175ffcc7aa18fb0ec2a04342c1
Alexander Malei <alexander.malei@innowise-group.com>
2020-01-24 13:44:33 +0300

    fix: task details header style

--- taskBoard/src/components/TaskDetails/Header/component.js
+++ taskBoard/src/components/TaskDetails/Header/component.js
@@ -1,7 +1,28 @@
 import styled from 'styled-components'
 
 export const HeaderContainer = styled.View`
+  flex: 1;
   margin-bottom: 20px;
+`
+
+export const TaskCreationContainer = styled.View`
+  flex: 1;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+`
+
+export const Author = styled.Text`
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  font-size: ${({ theme }) => theme.fontSizes[2]};
+`
+
+export const Date = styled.Text`
+  font-weight: ${({ theme }) => theme.fontWeights.regular};
+  font-size: ${({ theme }) => theme.fontSizes[2]};
+
+  text-transform: lowercase;
 `
 
 export const TaskName = styled.Text`
@@ -9,9 +30,4 @@ export const TaskName = styled.Text`
 
   font-weight: ${({ theme }) => theme.fontWeights.bold};
   font-size: ${({ theme }) => theme.fontSizes[4]};
-`
-
-export const CreationData = styled.Text`
-  font-weight: ${({ theme }) => theme.fontWeights.regular};
-  font-size: ${({ theme }) => theme.fontSizes[2]};
 `
--- taskBoard/src/components/TaskDetails/Header/index.js
+++ taskBoard/src/components/TaskDetails/Header/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
 import { parseDate } from '@/helpers'
-import { HeaderContainer, TaskName, CreationData } from './component'
+import { HeaderContainer, TaskName, TaskCreationContainer, Author, Date } from './component'
 
 //@todo: add icons on top of screen
 const Header = ({ name, authorName, creationDate }) => {
   return (
     <HeaderContainer>
       <TaskName>{name}</TaskName>
-      <CreationData>{`Added by ${authorName}  ${parseDate(creationDate)}`}</CreationData>
+      <TaskCreationContainer>
+        <Author>{`Added by ${authorName}, `}</Author>
+        <Date>{parseDate(creationDate)}</Date>
+      </TaskCreationContainer>
     </HeaderContainer>
   )
 }
