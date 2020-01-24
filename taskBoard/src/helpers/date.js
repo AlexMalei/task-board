44780cc14ceb81d1e2226fdd61b5f6ba85219328
Alexander Malei <alexander.malei@innowise-group.com>
2020-01-24 13:46:44 +0300

    fix: capitalize date messages

--- taskBoard/src/helpers/date.js
+++ taskBoard/src/helpers/date.js
@@ -2,11 +2,11 @@ import moment from 'moment'
 
 export const parseDate = dateString => {
   return moment(dateString).calendar(null, {
-    sameDay: '[today at] HH:mm',
-    nextDay: '[tomorrow at] HH:mm',
+    sameDay: '[Today at] HH:mm',
+    nextDay: '[Tomorrow at] HH:mm',
     nextWeek: 'dddd [at] HH:mm',
-    lastDay: '[yesterday at] HH:mm',
-    lastWeek: '[last] dddd [at] HH:mm ',
+    lastDay: '[Yesterday at] HH:mm',
+    lastWeek: '[Last] dddd [at] HH:mm ',
     sameElse: 'DD.MM.YYYY [at] HH:mm',
   })
 }
