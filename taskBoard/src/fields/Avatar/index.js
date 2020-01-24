9064b839f436068e78c1890835fb7a9d5389cf4c
Alexander Malei <alexander.malei@innowise-group.com>
2020-01-24 17:39:39 +0300

    fix: use normalize & fix participant avatars on task details

--- taskBoard/src/fields/Avatar/index.js
+++ taskBoard/src/fields/Avatar/index.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import { Avatar as ReactAvatar } from 'react-native-elements'
+import normalize from 'react-native-normalize'
 
 import { getInitials } from '@/utils'
 import { theme } from '@/theme'
 
 const DEFAULT_USER_NAME = 'No Name'
 
-const Avatar = ({ avatarUrl, userName, size, onPickImage, onHandleUploadAvatar, update, containerStyle }) => {
+const Avatar = ({
+  avatarUrl,
+  userName,
+  size,
+  onPickImage,
+  onHandleUploadAvatar,
+  update,
+  containerStyle,
+  avatarStyle,
+}) => {
+  console.log('size', size)
   return avatarUrl ? (
     <ReactAvatar
       containerStyle={containerStyle}
+      avatarStyle={avatarStyle}
       onPress={() => update && onPickImage(onHandleUploadAvatar)}
       rounded
-      size={size}
+      size={normalize(size)}
       source={{ uri: avatarUrl }}
     />
   ) : (
@@ -24,6 +36,7 @@ Avatar.defaultProps = {
   size: theme.avatarSizes.medium,
   edit: false,
   containerStyle: {},
+  avatarStyle: {},
   userName: DEFAULT_USER_NAME,
 }
 
